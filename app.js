4c00a6566d3c11d2a37532eb6eee0a4c79b83757
Mara Green <maragreen42@gmail.com>
2022-12-08 20:59:24 -0800

    creat findIngrediants function

--- app.js
+++ app.js
@@ -188,9 +188,27 @@ console.log("These dishes have an even number of servings:", evenNumbers)
 //5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
 //Filter
 
+function findIngrediants() {
+  let results = dishes.filter(function(el){
+    if(el.ingredients.includes("tomato") & el.ingredients.includes("cheese")){
+      return true;
+    }else {
+      return false;
+    }
+  })
+  return results
+}
+let ingredients = findIngrediants()
+console.log('Dishes with tomato and cheese:', ingredients)
+
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
+function printCuisineNames() {
+  let results = dishes.map(function(el){
+    
+  })
 
+}
 // BONUS: (come back to this after finishing all)
 //6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
